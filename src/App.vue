2b24a44d1201de4e39c417ba324fd5118a1f3b52
Daniel Cruz <nox.sb@hotmail.com>
2019-10-04 00:32:13 -0500

    basic layout for expense tracker both mobile and desktop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="desktop-layout">
+      <div class="left-navbar">
+        <ul class="tabs-container">
+          <li class="tab-option">Resumen</li>
+          <li class="tab-option">Cuentas</li>
+          <li class="tab-option">Ingresos</li>
+          <li class="tab-option">Gastos</li>
+        </ul>
+      </div>
+      <div class="main-content">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </div>
+      <BottomNavbar />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import BottomNavbar from "./components/BottomNavbar.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    BottomNavbar
   }
-}
+};
 </script>
 
-<style>
+<style >
+body {
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.desktop-layout {
+  display: grid;
+  grid-template-columns: 250px 1fr;
+  height: 100vh;
+}
+
+.left-navbar {
+  background: #6cb2eb;
+}
+
+.tabs-container {
+  list-style: none;
+  font-weight: 500;
+  color: #eff8ff;
+  margin: 0;
+  padding: 0;
+}
+
+.tab-option {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 70px;
+  transition: 0.1s ease-in;
+}
+.tab-option:hover {
+  cursor: pointer;
+  background: #3490dc;
+}
+@media (max-width: 1024px) {
+  .left-navbar {
+    display: none;
+  }
+  .desktop-layout {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
---
+++ src/components/BottomNavbar.vue
@@ -0,0 +1,83 @@
+<template>
+  <nav>
+    <div class="button-container">
+      <div class="add-button">+</div>
+    </div>
+    <section class="main-tabs">
+      <ul class="tabs-container-mobile">
+        <li class="tab-option-mobile">Resumen</li>
+        <li class="tab-option-mobile">Cuentas</li>
+        <li class="tab-option-mobile">Ingresos</li>
+        <li class="tab-option-mobile">Gastos</li>
+      </ul>
+    </section>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BottomNavbar"
+};
+</script>
+
+<style>
+nav {
+  display: none;
+  position: fixed;
+  border-radius: 15px 15px 0 0;
+  background: #6cb2eb;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+}
+
+.button-container {
+  position: relative;
+}
+
+.add-button {
+  position: absolute;
+  left: 50%;
+  top: -25px;
+  cursor: pointer;
+  box-shadow: 0px 5px 20px 0px rgba(28, 61, 90, 1);
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 1.5rem;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: #2779bd;
+}
+
+.tabs-container-mobile {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  font-weight: 500;
+  color: #eff8ff;
+  justify-content: space-around;
+}
+.tab-option-mobile {
+  height: 100%;
+  padding-top: 30px;
+}
+
+.main-tabs,
+.tabs-container-mobile,
+.tab-option-mobile {
+  height: 100%;
+}
+
+@media (max-width: 1024px) {
+  nav {
+    display: initial;
+  }
+}
+</style>
